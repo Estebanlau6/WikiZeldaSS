510c0c31073e197b67d6b59bc40724e54921a837
Estebanlau6 <laurentesteban321@gmail.com>
2025-09-17 12:31:53 +0200

    Premiet comit

---
+++ WikiZeldaSS/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WikiZeldaSS")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WikiZeldaSS.Pages")]
---
+++ WikiZeldaSS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WikiZeldaSS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
