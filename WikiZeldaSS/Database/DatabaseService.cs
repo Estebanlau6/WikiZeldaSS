b30a63b38b88af8040c138963e6e3255fbdc5de6
Estebanlau6 <laurentesteban321@gmail.com>
2025-10-29 16:17:38 +0100

    supp bug

--- WikiZeldaSS/Database/DatabaseService.cs
+++ WikiZeldaSS/Database/DatabaseService.cs
@@ -312,13 +312,6 @@ namespace WikiZeldaSS.Database
         public ObservableCollection<Objet> Objets { get; set; }
         public ObservableCollection<Quete> Quetes { get; set; }
         public ObservableCollection<Lieu> Lieux { get; set; }
-        public ObservableCollection<LieuxDetail> LieuxDetails { get; set; }
-        public ObservableCollection<ObjetsDetail> ObjetsDetails { get; set; }
-        public ObservableCollection<PersonnesDetail> PersonnesDetails { get; set; }
-        public ObservableCollection<QuetesDetail> QuetesDetails { get; set; }
-
-
-
     }
 
 }
